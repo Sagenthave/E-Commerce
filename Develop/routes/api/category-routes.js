90b1da78b696cdd61486ec0026ade9580a9d3867
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-17 17:53:43 -0400

    fixing bug?

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -44,13 +44,13 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
-    res.status(200),json(categoryData);
+    res.status(200).json(categoryData);
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
-// UPDATE A CATEGORY (REVIEW WITH TA)
+// UPDATE A CATEGORY 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
